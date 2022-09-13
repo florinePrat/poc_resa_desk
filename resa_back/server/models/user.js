13fb25ac9f3d3af2257fab7ca5bd3907decdd1b3
florinePrat <florineptlg64@gmail.com>
2022-09-13 18:41:40 +0200

    add department user model

--- resa_back/server/models/user.js
+++ resa_back/server/models/user.js
@@ -4,10 +4,11 @@ const userSchema = mongoose.Schema({
     name: { type: String, require: true },
     mail: { type: String, require: true },
     password: { type: String, require: true },
+    department: { type: String},
     bookingList: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking' },
     notification: { type: Boolean, default: false },
     resp: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    avatar: { type: String, require: true },
+    avatar: { type: String},
 });
 console.log('user model created')
 module.exports = mongoose.model('User', userSchema);
